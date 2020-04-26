3849c60b462a3e10336c6e09832b95deec2c6308
ncchen99 <ncchen99@gmail.com>
2020-04-26 19:59:39 +0800

    0426

--- login/index.php
+++ login/index.php
@@ -4,7 +4,7 @@
 	<title>Login</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
+<!--===============================================================================================-->
 	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
@@ -14,7 +14,7 @@
 	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-<!--===============================================================================================-->	
+<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
@@ -24,7 +24,7 @@
 <!--===============================================================================================-->
 </head>
 <body>
-	
+
 	<div class="limiter">
 		<div class="container-login100" style="background-image: url('images/img-01.jpg');">
 			<div class="wrap-login100 p-t-190 p-b-30">
@@ -34,12 +34,7 @@
 					</div>
 					<span class="login100-form-title p-t-20 p-b-45">
 						<?php
-							 
-							if(!isset($_SESSION['loginWrong'])){			
-								echo 'user';
-							}else{
-								echo 'username or password wrong';
-							}	
+							echo 'user';
 						?>
 					</span>
 
@@ -74,7 +69,7 @@
 					<div class="text-center w-full">
 						<a class="txt1" href="#">
 							Create new account
-							<i class="fa fa-long-arrow-right"></i>						
+							<i class="fa fa-long-arrow-right"></i>
 						</a>
 					</div>
 					-->
@@ -82,11 +77,11 @@
 			</div>
 		</div>
 	</div>
-	
-	
 
-	
-<!--===============================================================================================-->	
+
+
+
+<!--===============================================================================================-->
 	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
 	<script src="vendor/bootstrap/js/popper.js"></script>
